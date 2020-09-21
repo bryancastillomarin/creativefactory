5aba7349651415efdcee663a020fa7248510bb2e
bryan201-git <bryan_cm96@hotmail.com>
2020-09-21 18:15:19 -0500

    se hizo la reorganizacion de archivos en PHP

---
+++ footer.php
@@ -0,0 +1,37 @@
+<?php
+?>
+
+	<div class="clearfix"></div>
+
+	<footer>
+		<div id="piedepagina">
+			<p>Diseño creativo web Creative Factory México 2020</p>
+			<h2 id="imagenfooter">Imagen Creative</h2>
+		</div>
+		<div class="clearfix"></div>
+		<ul id="listapie">
+            <li>
+				<a href="#">Términos y Aviso de privacidad</a>
+            </li>
+            <li class="colorazulclaro">
+				|
+            </li>
+            <li>
+				<a href="#">Acércate</a>
+			</li>
+			<li class="colorazulclaro">
+				|
+			</li>
+			<li>
+			 <a href="#"> Ayuda </a>
+			</li>
+			<li>
+				2017-2020,Le Groupe Premier México, SAS o sus filiales.
+			</li>
+		</ul>
+	</footer>
+</body>
+</html>
+
+<?php
+?>
---
+++ header.php
@@ -0,0 +1,45 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+	<div id="cabecera">
+		<nav>
+			<ul>
+				<li><a href="#">inicio</a></li>
+				<li><a href="#">somos</a></li>
+				<li><a href="#">soluciones</a></li>
+				<li><a href="#">ética</a></li>
+				<li><a href="#">acércate</a></li>
+				<li><a href="#">news</a></li>
+			</ul>
+		</nav>
+		<header>
+			<h1 id="titulo">Creative Factory</h1>
+			publicidad&marketing
+		</header>
+	</div>
+
+	<div class="clearfix"></div>
+
+	<div id="barracolores">
+		<div id="azuloscuro">Creative Factory</div>
+		<div id="verdelimon">Creative Factory</div>
+		<div id="azulcielo">Creative Factory</div>
+		<div id="lila">Creative Factory</div>
+		<div id="rosado">Creative Factory</div>
+		<div id="rojo">Creative Factory</div>
+		<div id="naranja">Creative Factory</div>
+		<div id="amarillo">Creative Factory</div>
+	</div>
+
+
+<?php
+?>
