4efce93fb46f851770b590fec52c0784d8e4ff77
bryan201-git <bryan_cm96@hotmail.com>
2020-10-08 19:13:07 -0500

    cambio en las noticias y el slider para llamar a la BD

--- include/content/contentNews.php
+++ include/content/contentNews.php
@@ -1,3 +1,17 @@
+<?php
+	include "admin/db/conexion.php";
+	$sql = "SELECT * FROM news";
+	if ($resultado = $db->query($sql)) {
+		while ($fila = $resultado->fetch_row()) {
+			$encabezado = $fila[1];
+			$imagen = "img/noticias/".$fila[2];
+			$texto = $fila[3];
+		}
+	} else {
+		echo "Error de conexion";
+	}
+?>
+
 	<section class="content">
 		<div class="imagenprincipal">
 			<img src="img/contenido/news.jpg">
@@ -5,6 +19,20 @@
 		
 		<div class="frase">
 			<img src="img/frases/07CFWebFrase.jpg">
+		</div>
+
+		<div class="noticia">
+			<div class="noticia-encabezado">
+				<h3><?php echo $encabezado; ?></h3>
+			</div>
+			<div class="noticia-imagen">
+				<img src=<?php echo "\"".$imagen."\""; ?> >
+			</div>
+			<div class="noticia-texto">
+				<p>
+					<?php echo $texto; ?>
+				</p>
+			</div>
 		</div>
 		
 	</section>
--- include/slider.php
+++ include/slider.php
@@ -1,46 +1,83 @@
+<?php
+    include "admin/db/conexion.php";
+    $sql = "SELECT * FROM sliders";
+    $contador = 0;
+    
+?>
+
 <div class="clearfix"></div>
     <div id="slider">
         <div class="w3-content w3-display-container">
+            <?php 
+                if ($resultado = $db->query($sql)) {
+                    while ($fila = $resultado->fetch_row()) {
+                        $imagen = "img/sliders/".$fila[1];
+                        $url = $fila[2];
+                        $contador++;
+            ?>
+            <img class="mySlides" src=<?php echo "\"".$imagen."\""; ?> >
+            <?php
+                    }
+                } else {
+                    echo "Error de conexion";
+                }
+            ?>
+            <!--
             <img class="mySlides" src="img/sliders/slider1covid19.jpg">
             <img class="mySlides" src="img/sliders/slider2cocina.jpg">
             <img class="mySlides" src="img/sliders/slider3teotihuacan.jpg">
             <img class="mySlides" src="img/sliders/slider4RR.jpg">
+            -->
             <div class="w3-center w3-display-bottommiddle" style="width:100%">
                 <div class="w3-left" onclick="plusDivs(-1)">&#10094;</div>
+                <?php 
+                    for ($i=0; $i < $contador; $i++) { 
+                ?>
+                <span class="w3-badge demo w3-border" onclick=<?php echo "\"currentDiv(".($i + 1).")\""; ?> ></span>
+                <?php
+                    }
+                ?>
+                <!--
                 <span class="w3-badge demo w3-border" onclick="currentDiv(1)"></span>
                 <span class="w3-badge demo w3-border" onclick="currentDiv(2)"></span>
                 <span class="w3-badge demo w3-border" onclick="currentDiv(3)"></span>
                 <span class="w3-badge demo w3-border" onclick="currentDiv(4)"></span>
+                -->
                 <div class="w3-right" onclick="plusDivs(1)">&#10095;</div>
             </div>
         </div>
     </div>
+
 <script>
 
-var slideIndex = 1;
-showDivs(slideIndex);
+    var slideIndex = 1;
+    showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
 
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
+    function currentDiv(n) {
+        showDivs(slideIndex = n);
+    }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
-  }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
-}
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        if (n > x.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = x.length
+        }
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" w3-white", "");
+        }
+        x[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " w3-white";
+    }
 </script>
